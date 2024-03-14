69c662e03d6538e09cef0cbab0a9e16c09b1f717
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-03-14 16:50:24 +0500

    Writing some dcumentation

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res,) {
   res.render('index', { title: req.flash('Message') });
 });
+
+//You have to set value of flash as key pair form
+// Just as  flash("Name","Value")
+// And when you want to use that value just write
+//req.flash("Name")
+//And the important thing is flash message is working on session so you have 
+//to install session package first to procede further
+
 router.get('/flash', function(req,res){
   req.flash("Message","Hy i am value from flash page")
   res.render('flash')
